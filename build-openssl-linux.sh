00aa2ec4d9c7b48fc18472fa3723cb56be9058ac
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-09 12:33:22 +0500

    Update ZLIB paths for aarch64 architecture

--- build-openssl-linux.sh
+++ build-openssl-linux.sh
@@ -42,8 +42,8 @@ for ARCH in x86_64 arm64; do
         export AR=aarch64-linux-gnu-ar
         export RANLIB=aarch64-linux-gnu-ranlib
         export LD=aarch64-linux-gnu-ld
-        export ZLIB_LIBRARY=/usr/aarch64-linux-gnu/lib/libz.so
-        export ZLIB_INCLUDE_DIR=/usr/aarch64-linux-gnu/include
+        export ZLIB_LIBRARY=/usr/lib/aarch64-linux-gnu/libz.so
+        export ZLIB_INCLUDE_DIR=/usr/include/aarch64-linux-gnu
     else
         CONFIGURE_TARGET="linux-x86_64"
     fi
