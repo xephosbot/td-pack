29fcd861f296ff32816efc3ac6f32b9fc32e00d4
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-10 13:35:01 +0500

    Update build-openssl-linux.sh

--- build-openssl-linux.sh
+++ build-openssl-linux.sh
@@ -33,25 +33,29 @@ for ARCH in x86_64 arm64; do
 
     make distclean >/dev/null 2>&1 || true
 
-    CONFIGURE_TARGET=""
-   if [ "$ARCH" == "arm64" ]; then
-        CC_CMD="clang-18 --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu"
-        CXX_CMD="clang++-18 --target=aarch64-linux-gnu --sysroot=/usr/aarch64-linux-gnu"
-        CONFIGURE_TARGET="linux-aarch64"
+    if [ "$ARCH" == "arm64" ]; then
+        TARGET="linux-aarch64"
+        CC_CMD="aarch64-linux-gnu-gcc"
+        CXX_CMD="aarch64-linux-gnu-g++"
+        AR_CMD="aarch64-linux-gnu-ar"
+        RANLIB_CMD="aarch64-linux-gnu-ranlib"
     else
-        CC_CMD="clang-18"
-        CXX_CMD="clang++-18"
-        CONFIGURE_TARGET="linux-x86_64"
+        TARGET="linux-x86_64"
+        CC_CMD="gcc"
+        CXX_CMD="g++"
+        AR_CMD="ar"
+        RANLIB_CMD="ranlib"
     fi
 
-    ./Configure "$CONFIGURE_TARGET" \
+    ./Configure "$TARGET" \
         --prefix="$INSTALL_PATH" \
         --openssldir="$INSTALL_PATH" \
+        no-shared no-tests -fPIC \
         CC="$CC_CMD" \
         CXX="$CXX_CMD" \
-        AR="llvm-ar-18" \
-        RANLIB="llvm-ranlib-18" \
-        no-shared no-tests -fPIC >/dev/null || exit 1
+        AR="$AR_CMD" \
+        RANLIB="$RANLIB_CMD" \
+        >/dev/null || exit 1
 
     make -j"$(nproc)" >/dev/null || exit 1
     make install_sw >/dev/null || exit 1
