1a49b411572152202556fe16cf6b890a1883fee1
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-11 16:01:27 +0500

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -10,18 +10,20 @@ ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # ensure conan dependencies
 if [ "$ARCH" = "x86_64" ]; then
-  conan profile detect --force --name temp_detect || true
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_x86_64 --build=missing
+  source build/conan/Release/generators/conanbuild.sh
   cmake --preset linux-x86_64
   cmake --build --preset build-linux-x86_64-install
 else
   # prepare generated files using native build
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_x86_64 --build=missing
+  source build/conan/Release/generators/conanbuild.sh
   cmake --preset linux-x86_64
   cmake --build --preset build-linux-aarch64-prepare
   
   # install cross deps (zlib/openssl) for aarch64 via conan
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_aarch64 --build=missing
+  source build/conan/Release/generators/conanbuild.sh
   cmake --preset linux-aarch64
   cmake --build --preset build-linux-aarch64-install
 fi
