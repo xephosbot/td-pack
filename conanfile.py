23003efadd45e362965fd07f985515660c1deb19
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-11 15:02:56 +0500

    Create conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+
+class TdlibConan(ConanFile):
+	name = "tdlib-repo"
+	version = "1.0"
+
+	settings = "os", "arch", "compiler", "build_type"
+	generators = "CMakeDeps", "CMakeToolchain"
+
+	requires = (
+		"openssl/1.1.1w",
+		"zlib/1.2.13"
+	)
+
+	options = {"shared": [True, False]}
+	default_options = {"shared": False}
+
+	def layout(self):
+		pass
+
+	def generate(self):
+		tc = CMakeToolchain(self)
+		# keep install prefix controlled by CMakePresets.json
+		tc.generate()
+		deps = CMakeDeps(self)
+		deps.generate()
+
+# We intentionally don't implement build() to force user to use CMake presets
+# The conanfile is used to fetch and provide dependencies via CMakeToolchain + CMakeDeps
