4e8dd5299db394420d22bfbc0915508dd014b26b
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-11 15:44:50 +0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,24 @@
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
-class TdlibConan(ConanFile):
-	name = "tdlib-repo"
-	version = "1.0"
+class TdLibRecipe(ConanFile):
+    name = "tdlib-repo"
+    version = "1.0"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = ("CMakeDeps", "CMakeToolchain")
 
-	settings = "os", "arch", "compiler", "build_type"
-	generators = "CMakeDeps", "CMakeToolchain"
-
-	requires = (
-		"openssl/1.1.1w",
-		"zlib/1.2.13"
-	)
-
-	options = {"shared": [True, False]}
-	default_options = {"shared": False}
+    def requirements(self):
+        self.requires("openssl/1.1.1w")
+        self.requires("zlib/1.2.13")
 
     def layout(self):
         cmake_layout(self, build_folder="build/conan")
 
-	def generate(self):
-		tc = CMakeToolchain(self)
-		# keep install prefix controlled by CMakePresets.json
-		tc.generate()
-		deps = CMakeDeps(self)
-		deps.generate()
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
-# We intentionally don't implement build() to force user to use CMake presets
-# The conanfile is used to fetch and provide dependencies via CMakeToolchain + CMakeDeps
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
