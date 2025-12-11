e4563d8864e54924b9293f4fa6e9b277131751da
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-11 15:37:29 +0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class TdlibConan(ConanFile):
 	default_options = {"shared": False}
 
 	def layout(self):
-    	cmake_layout(self, build_folder="build/conan")
+        cmake_layout(self, build_folder="build/conan")
 
 	def generate(self):
 		tc = CMakeToolchain(self)
