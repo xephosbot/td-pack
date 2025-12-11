34b3da391bf47d0aef63614e86b47bacb79d54df
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-11 16:42:37 +0500

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -12,24 +12,18 @@ ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 if [ "$ARCH" = "x86_64" ]; then
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_x86_64 --build=missing
   source build/conan/Release/generators/conanbuild.sh
-  echo "Using C compiler: $CC"
-  echo "Using CXX compiler: $CXX"
   cmake --preset linux-x86_64
   cmake --build --preset build-linux-x86_64-install
 else
   # prepare generated files using native build
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_x86_64 --build=missing
   source build/conan/Release/generators/conanbuild.sh
-  echo "Using C compiler: $CC"
-  echo "Using CXX compiler: $CXX"
   cmake --preset linux-x86_64
   cmake --build --preset build-linux-aarch64-prepare
   
   # install cross deps (zlib/openssl) for aarch64 via conan
   conan install . -pr:b=conan_profiles/linux_x86_64 -pr:h=conan_profiles/linux_aarch64 --build=missing
   source build/conan/Release/generators/conanbuild.sh
-  echo "Using C compiler: $CC"
-  echo "Using CXX compiler: $CXX"
   cmake --preset linux-aarch64
   cmake --build --preset build-linux-aarch64-install
 fi
