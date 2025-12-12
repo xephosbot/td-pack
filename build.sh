743fa16d75a7f505fe9282521cbb6868fd952747
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-12 14:22:48 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,54 +16,17 @@ echo "Building for OS: $OS, ARCH: $ARCH"
 if [ "$OS" = "linux" ]; then
   if [ "$ARCH" = "x86_64" ]; then
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
-
-
-    GEN_DIR="build/conan/Release/generators"
-    
-    echo "====== conan_toolchain.cmake ======"
-    if [ -f "$GEN_DIR/conan_toolchain.cmake" ]; then
-        cat "$GEN_DIR/conan_toolchain.cmake"
-    else
-        echo "Файл не найден: $GEN_DIR/conan_toolchain.cmake"
-    fi
-    
-    echo "====== CMakePresets.json ======"
-    if [ -f "$GEN_DIR/CMakePresets.json" ]; then
-        cat "$GEN_DIR/CMakePresets.json"
-    else
-        echo "Файл не найден: $GEN_DIR/CMakePresets.json"
-    fi
-
-    
-    cmake --preset linux-x86_64
-    cmake --build --preset build-linux-x86_64-install
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target install
   elif [ "$ARCH" = "arm64" ] || [ "$ARCH" = "aarch64" ]; then
     # prepare generated files using native build
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
-
-    GEN_DIR="build/conan/Release/generators"
-    
-    echo "====== conan_toolchain.cmake ======"
-    if [ -f "$GEN_DIR/conan_toolchain.cmake" ]; then
-        cat "$GEN_DIR/conan_toolchain.cmake"
-    else
-        echo "Файл не найден: $GEN_DIR/conan_toolchain.cmake"
-    fi
-    
-    echo "====== CMakePresets.json ======"
-    if [ -f "$GEN_DIR/CMakePresets.json" ]; then
-        cat "$GEN_DIR/CMakePresets.json"
-    else
-        echo "Файл не найден: $GEN_DIR/CMakePresets.json"
-    fi
-
-    
-    cmake --preset linux-x86_64
-    cmake --build --preset build-linux-aarch64-prepare
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target prepare_cross_compiling
 
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_aarch64 --build=missing
-    cmake --preset linux-aarch64
-    cmake --build build-linux-aarch64-install
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target install
   else
     echo "Unsupported Linux architecture: $ARCH"
     exit 1
@@ -71,53 +34,17 @@ if [ "$OS" = "linux" ]; then
 elif [ "$OS" = "macos" ]; then
   if [ "$ARCH" = "arm64" ]; then 
     conan install . -pr:b=profiles/macos_arm64 -pr:h=profiles/macos_arm64 --build=missing
-
-    GEN_DIR="build/conan/Release/generators"
-    
-    echo "====== conan_toolchain.cmake ======"
-    if [ -f "$GEN_DIR/conan_toolchain.cmake" ]; then
-        cat "$GEN_DIR/conan_toolchain.cmake"
-    else
-        echo "Файл не найден: $GEN_DIR/conan_toolchain.cmake"
-    fi
-    
-    echo "====== CMakePresets.json ======"
-    if [ -f "$GEN_DIR/CMakePresets.json" ]; then
-        cat "$GEN_DIR/CMakePresets.json"
-    else
-        echo "Файл не найден: $GEN_DIR/CMakePresets.json"
-    fi
-    
-    cmake --preset macos-arm64
-    cmake --build --preset build-macos-arm64-install
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target install
   elif [ "$ARCH" = "x86_64" ]; then
     # prepare generated files using native build
     conan install . -pr:b=profiles/macos_arm64 -pr:h=profiles/macos_arm64 --build=missing
-
-    GEN_DIR="build/conan/Release/generators"
-    
-    echo "====== conan_toolchain.cmake ======"
-    if [ -f "$GEN_DIR/conan_toolchain.cmake" ]; then
-        cat "$GEN_DIR/conan_toolchain.cmake"
-    else
-        echo "Файл не найден: $GEN_DIR/conan_toolchain.cmake"
-    fi
-    
-    echo "====== CMakePresets.json ======"
-    if [ -f "$GEN_DIR/CMakePresets.json" ]; then
-        cat "$GEN_DIR/CMakePresets.json"
-    else
-        echo "Файл не найден: $GEN_DIR/CMakePresets.json"
-    fi
-
-    
-    cmake --preset macos-arm64
-    cmake --build --preset macos-arm64 --target prepare_cross_compiling
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target prepare_cross_compiling
   
     conan install . -pr:b=profiles/macos_arm64 -pr:h=profiles/macos_x86_64 --build=missing
-
-    cmake --preset macos-x86_64
-    cmake --build --preset build-macos-arm64-install
+    cmake --preset conan-release
+    cmake --build --preset conan-release --target install
   else
     echo "Unsupported macOS architecture: $ARCH"
     exit 1
