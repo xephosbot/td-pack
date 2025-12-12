24f2675cff6cacfc4e491cf4abce38060126b77c
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-12 09:30:02 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,16 +16,19 @@ echo "Building for OS: $OS, ARCH: $ARCH"
 if [ "$OS" = "linux" ]; then
   if [ "$ARCH" = "x86_64" ]; then
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
+    source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-x86_64
     cmake --build --preset build-linux-x86_64-install
   elif [ "$ARCH" = "arm64" ] || [ "$ARCH" = "aarch64" ]; then
     # prepare generated files using native build
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
+    source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-x86_64
     cmake --build --preset build-linux-aarch64-prepare
 
     # install cross deps (zlib/openssl) for aarch64 via conan
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_aarch64 --build=missing
+    source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-aarch64
     cmake --build --preset build-linux-aarch64-install
   else
@@ -35,10 +38,12 @@ if [ "$OS" = "linux" ]; then
 elif [ "$OS" = "macos" ]; then
   if [ "$ARCH" = "arm64" ]; then
     conan install . -pr:b=profiles/macos_arm64 -pr:h=profiles/macos_arm64 --build=missing
+    source build/conan/Release/generators/conanbuild.sh
     cmake --preset macos-arm64
     cmake --build --preset build-macos-arm64-install
   elif [ "$ARCH" = "x86_64" ]; then
     conan install . -pr:b=profiles/macos_arm64 -pr:h=profiles/macos_x86_64 --build=missing
+    source build/conan/Release/generators/conanbuild.sh
     cmake --preset macos-x86_64
     cmake --build --preset build-macos-x86_64-install
   else
