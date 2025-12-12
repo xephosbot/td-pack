6ca70d46658226eb75f2a51365388456c3775cdf
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-12 10:32:17 +0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,9 @@ class TdLibRecipe(ConanFile):
         self.requires("openssl/1.1.1w")
         self.requires("zlib/1.3.1")
 
+    def build_requirements(self):
+        self.tool_requires("cmake/3.27.9")
+
     def layout(self):
         cmake_layout(self, build_folder="build/conan")
 
