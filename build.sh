a590603721c0b71a356d2de7243bc521724ffbc9
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-12-12 10:54:26 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,19 +18,19 @@ if [ "$OS" = "linux" ]; then
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
     source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-x86_64
-    cmake --build --preset build-linux-x86_64-install -S td
+    cmake --build --preset build-linux-x86_64-install
   elif [ "$ARCH" = "arm64" ] || [ "$ARCH" = "aarch64" ]; then
     # prepare generated files using native build
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_x86_64 --build=missing
     source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-x86_64
-    cmake --build --preset build-linux-aarch64-prepare -S td
+    cmake --build --preset build-linux-aarch64-prepare
 
     # install cross deps (zlib/openssl) for aarch64 via conan
     conan install . -pr:b=profiles/linux_x86_64 -pr:h=profiles/linux_aarch64 --build=missing
     source build/conan/Release/generators/conanbuild.sh
     cmake --preset linux-aarch64
-    cmake --build --preset build-linux-aarch64-install -S td
+    cmake --build --preset build-linux-aarch64-install
   else
     echo "Unsupported Linux architecture: $ARCH"
     exit 1
@@ -40,12 +40,12 @@ elif [ "$OS" = "macos" ]; then
     conan install . -pr:b=profiles/macos_x86_64 -pr:h=profiles/macos_arm64 --build=missing -c tools.apple:enable_bitcode=False
     source build/conan/Release/generators/conanbuild.sh
     cmake --preset macos-arm64
-    cmake --build --preset build-macos-arm64-install -S td
+    cmake --build --preset build-macos-arm64-install
   elif [ "$ARCH" = "x86_64" ]; then
-    conan install . -pr:b=profiles/macos_x86_64 -pr:h=profiles/macos_x86_64 --build=missing -c tools.apple:enable_bitcode=False
+    conan install . -pr:b=profiles/macos_x86_64 -pr:h=profiles/macos_x86_64 --build=missing
     source build/conan/Release/generators/conanbuild.sh
     cmake --preset macos-x86_64
-    cmake --build --preset build-macos-x86_64-install -S td
+    cmake --build --preset build-macos-x86_64-install
   else
     echo "Unsupported macOS architecture: $ARCH"
     exit 1
